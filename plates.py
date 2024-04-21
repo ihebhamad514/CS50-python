7249c300b6c340d1a2a5673efe5e9c9c36a65406
bb00x <ihebhamad514@gmail.com>
2024-04-21 09:55:01 +0100

    Update plates.py

--- plates.py
+++ plates.py
@@ -7,6 +7,19 @@ def main():
 
 
 def is_valid(s):
+    pos_c = -1 
+    pos_n = -1
+    for _ in s :
+        if not _.isalpha() and not _.isnumeric():
+            return False
+        elif _.isnumeric() :
+            pos_n = s.index(_)
+        elif _.isalpha() and pos_n != -1:
+            pos_c = s.index(_)
+        if pos_c > pos_n :
+            return False
+           
+
     if 2 <= len(s) <= 6 and s[0:2].isalpha() : 
         return True
     return False
